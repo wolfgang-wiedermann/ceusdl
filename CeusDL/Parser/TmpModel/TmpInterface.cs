e149217f926f111fe37c07b8ea78bf1805ae714d
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2016-07-01 15:29:17 +0200

    Zwei Model-Klassen für das Ergebnis des 1. Parserdurchlaufs angefangen

---
+++ CeusDL/Parser/TmpModel/TmpInterface.cs
@@ -0,0 +1,7 @@
+namespace Kdv.CeusDL.Parser.TmpModel
+{
+    internal class TmpInterface {
+        public string Name {get;set;}
+        // TODO: hier gehts weiter !!!
+    }
+}
---
+++ CeusDL/Parser/TmpModel/TmpParserResult.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace Kdv.CeusDL.Parser.TmpModel
+{
+    internal class TmpParserResult {
+        public List<TmpInterface> Interfaces {get;set;}
+        // TODO: hier gehts weiter !!!
+    }
+}
