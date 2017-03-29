1f1e9c83312bebbc2c6ff7afecbd58af43f18945
wolfgang-wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2017-03-29 11:43:55 +0200

    Kleinere Änderungen und Korrekturen. Insb. Variable für Ladelaufnummer

--- CeusDL/Generator/BL/BaseLayerLoadGenerator.cs
+++ CeusDL/Generator/BL/BaseLayerLoadGenerator.cs
@@ -4,31 +4,9 @@ using System;
 
 namespace Kdv.CeusDL.Generator.BL {
     ///
-    /// Zweck: Generieren der Load-Statements (Insert und Update)
-    /// Beispiel für Insert und Update bei nicht historiserter nicht mandantenorientierter DimTable:
-    ///    
-    /// --
-    /// -- Insert-Statement
-    /// --
-    /// insert into AP_BL_D_Bewerberstatus (
-    ///	    Bewerberstatus_KNZ, Bewerberstatus_KURZBEZ, Bewerberstatus_LANGBEZ, Bewerberstatus_HISKEY_ID, 
-    /// 	T_Modifikation, T_Ladelauf_NR, T_Benutzer, T_System, T_Erst_DAT, T_Aend_DAT
-    /// )
-    /// select Bewerberstatus_KNZ, Bewerberstatus_KURZBEZ, Bewerberstatus_LANGBEZ, Bewerberstatus_HISKEY_ID, 
-    ///	   T_Modifikation, 1, SYSTEM_USER, 'H', GetDate(), GetDate()
-    /// from AP_BL_D_Bewerberstatus_VW
-    /// where T_Modifikation = 'I'
+    /// Zweck: Generieren der Load-Statements (Insert und Update bzw. Delete und Insert)
     ///
-    /// --
-    /// -- Update-Statement (Aktualisiert genau die Felder, die im Case für U geprüft werden!)
-    /// --
-    /// update b
-    /// set b.Bewerberstatus_KURZBEZ = v.Bewerberstatus_KURZBEZ,
-    /// b.Bewerberstatus_LANGBEZ = v.Bewerberstatus_LANGBEZ,
-    ///	b.Bewerberstatus_HISKEY_ID = v.Bewerberstatus_HISKEY_ID
-    /// from AP_BL_D_Bewerberstatus as b inner join AP_BL_D_Bewerberstatus_VW as v
-    /// on b.Bewerberstatus_ID = v.Bewerberstatus_ID
-    /// where v.T_Modifikation = 'U'
+    /// Achtung: Noch ohne Historisierung von Attributwerten!!!
     ///
     public class BaseLayerLoadGenerator : BaseLayerAbstractGenerator
     {
@@ -49,7 +27,7 @@ namespace Kdv.CeusDL.Generator.BL {
                 } else if(ifa.Type == InterfaceType.FACT_TABLE) {                    
                     code += $"-- BaseLayer Laden für FactTable {ifa.Name}\n";
                     code += GenerateFactDeleteCode(ifa, model);
-                    // Achtung: Hier wird auch die Version von Dim verwendet!
+                    // Achtung: Hier wird auch die Version von Dim verwendet, da das bist dato gleich ist!
                     code += GenerateDimInsertCode(ifa, model);                     
                 }
             }
@@ -81,9 +59,8 @@ namespace Kdv.CeusDL.Generator.BL {
                 }
                 code += $"    {GetAttributeName(attr)}";
             }   
-
-            // TODO: 1 als Ladelaufnummer ist natürlich noch Sch...
-            code += ",\n    T_Modifikation, 1, SYSTEM_USER, 'H', GetDate(), GetDate(), 'Insert bei Load BL'";
+            
+            code += ",\n    T_Modifikation, @ladelauf_nr, SYSTEM_USER, 'H', GetDate(), GetDate(), 'Insert bei Load BL'";
 
             code += $"\nfrom {GetViewName(ifa, model.Config)}\n";
             code += "where T_Modifikation = 'I'";
@@ -109,6 +86,7 @@ namespace Kdv.CeusDL.Generator.BL {
                 }
             }
             code += ",\n    t.T_Modifikation = 'U', t.T_Bemerkung = 'Update bei Load BL', t.T_Benutzer = SYSTEM_USER, t.T_Aend_Dat = GetDate()";
+            code += ",\n    t.T_Ladelauf_NR = @ladelauf_nr";
             code += $"\nfrom {GetTableName(ifa, model.Config)} as t inner join {GetViewName(ifa, model.Config)} as v ";            
             code += $"\non t.{ifa.Name}_ID = v.{ifa.Name}_ID";
             code += $"\nwhere v.T_Modifikation = 'U'";
@@ -143,7 +121,7 @@ namespace Kdv.CeusDL.Generator.BL {
                 if(ifa.IsMandantInterface()) {
                     code += "  where Mandant_KNZ = @mandant\n";
                 } 
-                code += ")\n\n\n";
+                code += ")\n\n";
             }
             return code;
         }
@@ -151,7 +129,9 @@ namespace Kdv.CeusDL.Generator.BL {
         private string GenerateMandantParamCode()
         {
             string code = "DECLARE @mandant varchar(10);\n";
-            code += "SET @mandant = '0000';\n\n";
+            code += "DECLARE @ladelauf_nr int;\n";
+            code += "SET @mandant = '0000';\n";
+            code += "SET @ladelauf_nr = 1;\n\n";
             return code;
         }
     }
